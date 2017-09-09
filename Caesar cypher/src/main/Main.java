abfc271744223928a34912976c269eebe7e98556
RedAiHi <31795897+RedAiHi@users.noreply.github.com>
2017-09-09 13:46:20 +0300

    Initial upload
    First version of Caesar Cypher added

---
+++ Caesar cypher/src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		ArrayList<String> input = new ArrayList<>();
+		String raw_input;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Input your string, which you wish to encode with Caesar algorythm");
+		raw_input = sc.nextLine();
+		System.out.println("Thank you for your input! \nProcessing, please hold on for a minute... \n");
+		
+		System.out.println("");
+		
+		System.out.println("Finished!");
+	}
+
+}
