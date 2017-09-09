c56f27900f17f1154ad5aa597e08698515fbb1c8
RedAiHi <31795897+RedAiHi@users.noreply.github.com>
2017-09-09 17:52:26 +0300

    Final commit for Caesar Algorythm
    Finished cypher logic and output

--- Caesar cypher/src/main/Main.java
+++ Caesar cypher/src/main/Main.java
@@ -7,9 +7,8 @@ public class Main {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		//ArrayList<String> input = new ArrayList<>();
 		ArrayList<String> abc = new ArrayList<>();
+		ArrayList<String> rezult = new ArrayList<>();
 		int enc = 0;
 		
 		String raw_input;
@@ -18,34 +17,55 @@ public class Main {
 		
 		//Initializing alphabet array
 		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-		
 		for (char c : alphabet)
 		{
 			abc.add(String.valueOf(c));
 		}
 		
-		
 		System.out.println("Input your string, which you wish to encode with Caesar algorythm");
 		raw_input = sc.nextLine();
-		System.out.println("Input your caesar algorythm modifier");
+		System.out.println("Input your modifier (any integer)");
 		enc = sc.nextInt();
+		sc.close();
 		
 		System.out.println("Thank you for your input! \nProcessing, please hold on for a moment... \n");
 		input_Arr = raw_input.toCharArray();
 		
 		System.out.println("The input we got is: " + raw_input);
-		System.out.println("Modifier: " + enc + "\nArray: ");
+		//System.out.println("Modifier: " + enc + "\nArray: ");
 		for(char c : input_Arr)
 		{
 			int index = abc.indexOf(String.valueOf(c));
-			System.out.println(c + " " + index);
+			//System.out.println(c + " " + index);
 			
+			try
+			{
+				index = index + enc;
+				abc.get(index);
+			}
+			catch(IndexOutOfBoundsException e)
+			{
+				index = index - abc.size();
+			}
 			
-			System.out.print("   |   " + "\n");
+			//System.out.print(c + "   |   " + abc.get(index) + "\n");
+			if(index >= 0)
+			{
+				rezult.add(abc.get(index));
+			}
+			else
+			{
+				rezult.add(" ");
+			}
 		}
-			
 		
-		System.out.println("Finished!");
+		System.out.println("Result:");
+		
+		for(String s : rezult)
+		{
+			System.out.print(s);
+		}
+		System.out.println("\nFinished!");
 	}
 
 }
